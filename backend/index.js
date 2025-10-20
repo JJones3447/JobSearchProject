f5bd1a7dba551b5abc1dd2957abd868646cff87d
Joshua Jones <blackchill47@gmail.com>
2025-10-20 11:34:50 -0500

    index.js can now start the server, added db.js and jobRoutes.js

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+require('dotenv').config();  
+const express = require('express'); 
+const cors = require('cors');
+const app = express();
+
+// const db = require('./config/db'); 
+
+app.use(cors());
+app.use(express.json());
+
+const jobRoutes = require('./routes/jobRoutes');
+
+app.use('/api/jobs', jobRoutes);
+
+app.get('/', (req, res) => {
+    res.send('Job Search API is up and running');
+});
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
